14d188f26d92ed1163007d942a2c3084b4b2817b
Erhan Fırat <erhanfirat@gmail.com>
2023-08-29 21:04:42 +0300

    footer update

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,7 +1,50 @@
+import { Link } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+
 const Footer = () => {
   return (
-    <footer className="header p-3">
-      <div>Sayfa Footerı</div>
+    <footer className="page-footer p-3">
+      <Container>
+        <Row>
+          <Col className="d-flex flex-column">
+            <h5>Tests</h5>
+            <Row className="border-top pt-2">
+              <Col className="d-flex flex-column">
+                <Link to="/">Sprint 01</Link>
+                <Link to="/">Sprint 02</Link>
+                <Link to="/">Sprint 03</Link>
+                <Link to="/">Sprint 04</Link>
+                <Link to="/">Sprint 05</Link>
+                <Link to="/">Sprint 06</Link>
+              </Col>
+              <Col className="d-flex flex-column">
+                <Link to="/">Sprint 07</Link>
+                <Link to="/">Sprint 08</Link>
+                <Link to="/">Sprint 09</Link>
+                <Link to="/">Sprint 10</Link>
+                <Link to="/">Sprint 11</Link>
+                <Link to="/">Sprint 12</Link>
+              </Col>
+              <Col className="d-flex flex-column">
+                <Link to="/">Sprint 13</Link>
+                <Link to="/">Sprint 14</Link>
+                <Link to="/">Sprint 15</Link>
+                <Link to="/">Sprint 16</Link>
+                <Link to="/">Sprint 17</Link>
+                <Link to="/">Sprint 18</Link>
+              </Col>
+              <Col className="d-flex flex-column">
+                <Link to="/">Sprint 19</Link>
+                <Link to="/">Sprint 20</Link>
+                <Link to="/">Sprint 21</Link>
+                <Link to="/">Sprint 22</Link>
+                <Link to="/">Sprint 23</Link>
+                <Link to="/">Sprint 24</Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
