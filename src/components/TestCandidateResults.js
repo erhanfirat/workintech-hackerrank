bf37cff0c620c8a06297e75f8a26fffd43771b9a
Erhan FIRAT <erhan.firat@cytosmart.com>
2023-09-01 16:30:23 +0300

    test candidates updates

--- src/components/TestCandidateResults.js
+++ src/components/TestCandidateResults.js
@@ -2,20 +2,42 @@ import { Col, Container, Row } from "reactstrap";
 
 export const TestCandidateResults = ({ test, candidates }) => {
   return (
-    <Container fluid>
+    <Container fluid className="test-candidate-results">
       <h3>Candidate Results Per Question</h3>
-      <Row>
-        <Col>{test?.name}</Col>
-        {test?.questions.map((question, i) => (
-          <Col key={`Q${i}`}>{i}</Col>
-        ))}
+      <Row className="border-bottom fw-bold">
+        <Col
+          sm="2"
+          className="text-truncate border-right border-end"
+          title={test?.name}
+        >
+          {test?.name}
+        </Col>
+        <Col sm="10">
+          <Row>
+            <Col className="">T %</Col>
+            {test?.questions.map((question, i) => (
+              <Col key={`Q${i}`} className="">{`Q${i + 1}`}</Col>
+            ))}
+          </Row>
+        </Col>
       </Row>
       {candidates?.map((candidate, j) => (
-        <Row key={`Candidate${j}`}>
-          <Col>{candidate.email}</Col>
-          {test.questions.map((question, i) => (
-            <Col key={`QA${i}`}>{candidate.questions[question]}</Col>
-          ))}
+        <Row key={`Candidate${j}`} className="border-bottom">
+          <Col
+            sm="2"
+            className="text-truncate border-end fw-bold"
+            title={candidate.email}
+          >
+            {candidate.email}
+          </Col>
+          <Col sm="10">
+            <Row>
+              <Col>{parseInt(candidate.percentage_score)}</Col>
+              {test.questions.map((question, i) => (
+                <Col key={`QA${i}`}>{candidate.questions[question]}</Col>
+              ))}
+            </Row>
+          </Col>
         </Row>
       ))}
     </Container>
