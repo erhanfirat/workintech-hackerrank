7bab85acd2f46c85bae0fc2c0692edce2c3ef6df
Erhan Fırat <erhanfirat@gmail.com>
2023-09-04 22:44:53 +0300

    change folder structure for backend implementation & adding .env

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const token =
-  "c6241c9f051397356b1e9432d226286ae42c134d7fb3d0aa5c752d6fc45ea955";
-
 export let API = axios.create({
   baseURL: "https://www.hackerrank.com/x/api/v3/",
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${process.env.REACT_APP_HACKERRANK_KEY}`,
   },
 });
 
