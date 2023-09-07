70c80635207879ff822ca2dc2d7dd49108fe66a7
Erhan FIRAT <erhan.firat@cytosmart.com>
2023-09-07 22:37:01 +0300

    server endpoints updated

--- frontend/src/api/srEndpoints.js
+++ frontend/src/api/srEndpoints.js
@@ -8,26 +8,14 @@ export const srEndpoints = {
    *
    * @returns tests list
    */
-  tests: (params = { limit: 100, offset: 0 }) => ({
-    initialData: [],
+  getTests: () => ({
     reqType: REQ_TYPES.GET,
     endpoint: "tests",
-    config: { params },
   }),
 
-  candidates: (testId, params = { limit: 100, offset: 0 }) => ({
-    initialData: [],
-    reqType: REQ_TYPES.GET,
-    endpoint: `tests/${testId}/candidates`,
-    config: { params },
-  }),
-  /** Returns parameters to fetch question object
-   * @param {String} questionId: id of question
-   * @returns axios get request parameters for the question
-   */
-  question: (questionId) => ({
-    initialData: null,
-    reqType: REQ_TYPES.GET,
-    endpoint: `questions/${questionId}`,
+  setAllTests: (tests) => ({
+    reqType: REQ_TYPES.POST,
+    endpoint: "tests",
+    payload: tests,
   }),
 };
