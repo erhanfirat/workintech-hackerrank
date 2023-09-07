5d0b53ecc121de43e8ecaf16a8ed29554c0d2fa5
Erhan FIRAT <erhan.firat@cytosmart.com>
2023-09-07 21:14:19 +0300

    test db crud

---
+++ server/db/testDB.js
@@ -0,0 +1,23 @@
+const knex = require("./knex");
+
+const createTest = (test) =>
+  knex("test").insert({ id: test.id, data: JSON.stringify(test) });
+
+const getAllTests = () => knex("test").select("*");
+
+const getTest = (testId) => knex("test").where("id", testId);
+
+const updateTest = (test) =>
+  knex("test")
+    .where("id", test.id)
+    .update({ id: test.id, data: JSON.stringify(test) });
+
+const deleteTest = (testId) => knex("test").where("id", testId).del();
+
+module.exports = {
+  createTest,
+  getAllTests,
+  getTest,
+  updateTest,
+  deleteTest,
+};
