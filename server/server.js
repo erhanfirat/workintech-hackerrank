528abe83a56afe45f19140753dadd8da4a32eb5f
Erhan FIRAT <erhan.firat@cytosmart.com>
2023-09-07 21:14:32 +0300

    test endpoints added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const testDB = require("./db/testDB");
 
-app.get("/", (req, res, next) => {
-  console.log("inside the request handler");
-  res.statusCode = 200;
-  res.send({ pi: Math.PI });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Test Endpoints
+
+app.get("/tests", async (req, res) => {
+  const testsRec = await testDB.getAllTests();
+  const tests = testsRec.map((tr) => JSON.parse(tr.data));
+  res.status(200).json({ tests });
+});
+
+app.get("/tests/:id", async (req, res) => {
+  const testRec = await testDB.getTest(req.params.id);
+  const test = JSON.parse(testRec.data);
+  res.status(200).json({ test });
+});
+
+app.post("/tests", async (req, res) => {
+  const createResult = await testDB.createTest(req.body);
+  res.status(201).json({ id: req.body.id });
+});
+
+app.put("/tests/:id", async (req, res) => {
+  const updateResult = await testDB.updateTest(req.body);
+  res.status(201).json({ id: req.body.id });
 });
 
 app.listen(3001);
