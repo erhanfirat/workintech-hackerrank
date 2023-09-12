af15746f5b2a86933266644845a548f06674dedb
Erhan Fırat <erhanfirat@gmail.com>
2023-09-12 03:49:27 +0300

    candidate endpoints added to server

---
+++ server/db/candidateDB.js
@@ -0,0 +1,27 @@
+const knex = require("./knex");
+
+const getAllCandidatesOfTest = (testId) =>
+  knex("candidate").select("*").where("test", testId);
+
+const upsertCandidate = (testId, candidate) => {
+  return knex.transaction(
+    async (trx) =>
+      await trx("candidate")
+        .insert({
+          id: candidate.id,
+          test: testId,
+          data: JSON.stringify(candidate),
+        })
+        .onConflict("id")
+        .merge()
+  );
+};
+
+const deleteCandidate = (testId) => knex("candidate").where("id", testId).del();
+
+module.exports = {
+  upsertCandidate,
+  getAllCandidatesOfTest,
+  getCandidate,
+  deleteCandidate,
+};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const testDB = require("./db/testDB");
+const candidateDB = require("./db/candidateDB");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,15 +18,25 @@ app.all("*", function (req, res, next) {
 });
 
 app.get("/tests", async (req, res) => {
-  const testsRec = await testDB.getAllTests();
-  const tests = testsRec.map((tr) => JSON.parse(tr.data));
-  res.status(200).json([...tests]);
+  try {
+    const testsRec = await testDB.getAllTests();
+    const tests = testsRec.map((tr) => JSON.parse(tr.data));
+    res.status(200).json(tests);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred", err });
+  }
 });
 
 app.get("/tests/:id", async (req, res) => {
-  const testRec = await testDB.getTest(req.params.id);
-  const test = JSON.parse(testRec.data);
-  res.status(200).json({ test });
+  try {
+    const testRec = await testDB.getTest(req.params.id);
+    const test = JSON.parse(testRec.data);
+    res.status(200).json(test);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred", err });
+  }
 });
 
 app.post("/tests", async (req, res) => {
@@ -38,7 +49,7 @@ app.post("/tests", async (req, res) => {
     } else {
       await testDB.upsertTest(bodyData);
     }
-    res.status(201).json({ id: req.body.id });
+    res.status(201).json(bodyData);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "An error occurred" });
@@ -54,6 +65,41 @@ app.put("/tests/:id", async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 });
+
+// Candidate Endpoints
+
+app.post("/tests/:testId/candidates", async (req, res) => {
+  try {
+    const bodyData = req.body;
+    const testId = req.params.testId;
+    if (bodyData instanceof Array) {
+      for (let i = 0; i < bodyData.length; i++) {
+        await candidateDB.upsertCandidate(testId, bodyData[i]);
+      }
+    } else {
+      await candidateDB.upsertCandidate(testId, bodyData);
+    }
+    res.status(201).json(bodyData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.get("/tests/:testId/candidates", async (req, res) => {
+  try {
+    const candidates = await candidateDB.getAllCandidatesOfTest(
+      req.params.testId
+    );
+    const test = JSON.parse(testRec.data);
+    res.status(200).json(test);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred", err });
+  }
+});
+
+// APP STARTS ON 3001
 
 app.listen(3001, () => {
   console.log("working on 3001");
