6bfa3e4cd640316895741af71d60a0acfa99df0e
Erhan Fırat <erhanfirat@gmail.com>
2023-09-16 01:40:57 +0300

    logger middleware added

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { reducers } from "./reducers";
 import thunk from "redux-thunk";
-// eski versiyonlar için -> import { createStore } from 'redux';
+import logger from "redux-logger";
 
-export const store = createStore(reducers, applyMiddleware(thunk));
+export const store = createStore(reducers, applyMiddleware(logger, thunk));
