7b7164cf30d458fcd432bc1767a5d17f2b2d8ca4
Erhan Fırat <erhanfirat@gmail.com>
2023-09-19 02:18:28 +0300

    app start logic improved with  getting groups

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 
 import { verifyUserAction } from "./store/reducers/userReducer";
+import { getAllGroupsActionCreator } from "./store/reducers/studentsReducer";
 import Main from "./layout/Main";
+import { FETCH_STATES } from "./utils/constants";
 
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector((s) => s.user.user);
+  const { groupsFetchState } = useSelector((s) => s.students);
 
   useEffect(() => {
     dispatch(verifyUserAction());
   }, []);
+
+  useEffect(() => {
+    if (user && groupsFetchState === FETCH_STATES.NOT_STARTED) {
+      dispatch(getAllGroupsActionCreator());
+    }
+  }, [user]);
 
   return (
     <BrowserRouter>
