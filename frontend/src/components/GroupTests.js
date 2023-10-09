5055eff3f11a6672fcdc2051d420ba76a16a5cdf
Erhan Fırat <erhanfirat@gmail.com>
2023-10-09 22:53:48 +0300

    group tests chart added

--- frontend/src/components/GroupTests.js
+++ frontend/src/components/GroupTests.js
@@ -1,7 +1,16 @@
 import { Col, Container, Row } from "reactstrap";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom/cjs/react-router-dom";
-import { useCallback } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const GroupTests = ({ group }) => {
   const testsInfo = useSelector((store) => {
@@ -16,11 +25,49 @@ const GroupTests = ({ group }) => {
     });
 
     return tests.map((t) => {
+      const { id, name } = t;
       const testInfo = groupTestsInfo.find((gti) => gti.test_id === t.id);
 
-      return { ...t, ...testInfo };
+      return {
+        id,
+        name,
+        shortName: name?.replace("[Workintech] - ", ""),
+        ...testInfo,
+        attendee_rate: (
+          (testInfo?.attendee_count / testInfo?.total_count) *
+          100
+        ).toFixed(0),
+      };
     });
   });
+
+  console.log(testsInfo);
+
+  const TooltipContent = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      console.log(payload);
+      return (
+        <div className="bg-white border rounded-1 p-3 shadow">
+          <h4>{label}</h4>
+          <Row className="border-top mt-2 pt-2">
+            <Col sm="6">{payload[0].name}:</Col>
+            <Col sm="6">{payload[0].value}%</Col>
+          </Row>
+          <Row className="border-top mt-2 pt-2">
+            <Col sm="6">{payload[1].name}:</Col>
+            <Col sm="6">{payload[1].value}%</Col>
+          </Row>
+          <Row>
+            <Col sm="6"></Col>
+            <Col sm="6">
+              {payload[1].payload.attendee_count} /
+              {payload[1].payload.total_count}
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+  };
 
   return (
     <Container fluid>
@@ -48,6 +95,42 @@ const GroupTests = ({ group }) => {
           </Col>
         </Row>
       ))}
+      <Row>
+        <Col style={{ height: "400px" }} className="pt-3">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={testsInfo}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="shortName" />
+              <YAxis />
+              <Tooltip content={<TooltipContent />} />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="average_score"
+                name="Ortalama"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line
+                type="monotone"
+                dataKey="attendee_rate"
+                stroke="#82ca9d"
+                name="Katılım"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </Col>
+      </Row>
     </Container>
   );
 };
